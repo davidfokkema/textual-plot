599ebd98663522e2a05fedfc739c63fba03a3000
David Fokkema <davidfokkema@icloud.com>
2025-01-19 17:01:06 +0100

    Implemented line plot

--- src/textual_plot/plot_widget.py
+++ src/textual_plot/plot_widget.py
@@ -1,7 +1,7 @@
 import random
 import time
 from dataclasses import dataclass
-from typing import Self
+from typing import Iterator, Self
 
 import numpy as np
 from numpy.typing import ArrayLike
@@ -20,7 +20,9 @@ class DataSet:
     y: np.ndarray
 
 
-class LinePlot(DataSet): ...
+@dataclass
+class LinePlot(DataSet):
+    line_style: Style
 
 
 @dataclass
@@ -44,8 +46,8 @@ class PlotWidget(Widget):
     def clear(self) -> None:
         self._datasets = []
 
-    def plot(self, x: ArrayLike, y: ArrayLike) -> None:
-        self._datasets.append(LinePlot(x=x, y=y))
+    def plot(self, x: ArrayLike, y: ArrayLike, line_style="white") -> None:
+        self._datasets.append(LinePlot(x=x, y=y, line_style=Style.parse(line_style)))
         self.refresh()
 
     def scatter(
@@ -68,6 +70,27 @@ class PlotWidget(Widget):
             *regions, repaint=repaint, layout=layout, recompose=recompose
         )
 
+    def _map_coordinate_to_pixel(self, x: float, y: float) -> tuple[float, float]:
+        def mapper(x, a, b, a_prime, b_prime):
+            return a_prime + (x - a) * (b_prime - a_prime) / (b - a)
+
+        return (
+            mapper(x, self._x_min, self._x_max, 0, self.size.width - 1),
+            mapper(y, self._y_min, self._y_max, 0, self.size.height - 1),
+        )
+
+    def _map_coordinates_to_pixels(
+        self, x: ArrayLike, y: ArrayLike
+    ) -> tuple[np.ndarray, np.ndarray]:
+        return (
+            np.interp(x, [self._x_min, self._x_max], [0, self.size.width - 1]).astype(
+                int
+            ),
+            np.interp(y, [self._y_min, self._y_max], [0, self.size.height - 1]).astype(
+                int
+            ),
+        )
+
     def _render_plot(self) -> None:
         self._plot_size = self.size
         if self._plot_size.width == 0:
@@ -77,12 +100,11 @@ class PlotWidget(Widget):
             [Segment(" ") for _ in range(self._plot_size.width)]
             for _ in range(self._plot_size.height)
         ]
-        # x_step = (x_max - x_min) / plot_size.width
         for dataset in self._datasets:
             if isinstance(dataset, ScatterPlot):
                 self._render_scatter_plot(dataset)
-            # x = np.arange(x_min + x_step / 2, x_max, x_step)
-            # assert len(x) == plot_size.width
+            elif isinstance(dataset, LinePlot):
+                self._render_line_plot(dataset)
 
     def _render_scatter_plot(self, dataset: ScatterPlot) -> None:
         x_cell_boundaries = np.linspace(
@@ -103,6 +125,50 @@ class PlotWidget(Widget):
                 # data point is outside plot area
                 pass
 
+    def _render_line_plot(self, dataset: LinePlot) -> None:
+        x, y = self._map_coordinates_to_pixels(dataset.x, dataset.y)
+
+        for i in range(1, len(x)):
+            for px, py in self._get_pixels_in_line(x[i - 1], y[i - 1], x[i], y[i]):
+                try:
+                    self._canvas[py][px] = Segment(text="█", style=dataset.line_style)
+                except IndexError:
+                    # data point is outside plot area
+                    self.notify(f"Out of bounds: ({px}, {py})")
+                    pass
+
+    def _get_pixels_in_line(
+        self, x0: int, y0: int, x1: int, y1: int
+    ) -> Iterator[tuple[int, int]]:
+        """Get all pixels that make up a line.
+
+        Args:
+            x0: starting point x cooridinate
+            y0: starting point y cooridinate
+            x1: end point x coordinate
+            y1: end point y coordinate
+
+        Yields:
+            Tuples of (x, y) coordinates that make up the line.
+        """
+        dy = y1 - y0
+        dx = x1 - x0
+
+        yield x0, y0
+        if dy < dx:
+            # mostly horizontal line, loop over x
+            a = dy / dx
+            b = -a * x0 + y0
+            for x in range(x0 + 1, x1):
+                yield x, round(a * x + b)
+        else:
+            # mostly vertical line, loop over y
+            a = dx / dy
+            b = -a * y0 + x0
+            for y in range(y0 + 1, y1):
+                yield round(a * y + b), y
+        yield x1, y1
+
     def render_line(self, y: int) -> Strip:
         """Render a line of the widget. y is relative to the top of the widget."""
         return Strip(
@@ -118,16 +184,18 @@ class DemoApp(App[None]):
         yield PlotWidget()
 
     def on_mount(self) -> None:
-        self.set_interval(1 / 60, self.plot_refresh)
+        self.set_interval(1 / 24, self.plot_refresh)
 
     def plot_refresh(self) -> None:
         plot = self.query_one(PlotWidget)
         plot.clear()
         plot.scatter(x=[0, 1, 2, 3, 4, 5], y=[0, 1, 4, 9, 16, 25], marker_style="blue")
-        x = np.linspace(0, 10, 1001)
-        plot.scatter(
-            x=x, y=10 + 10 * np.sin(x + self._phi), marker="█", marker_style="red3"
-        )
+        x = np.linspace(0, 10, 51)
+        # plot.scatter(
+        #     x=x, y=10 + 10 * np.sin(x + self._phi), marker="█", marker_style="red3"
+        # )
+        plot.plot(x=x, y=10 + 10 * np.sin(x + self._phi), line_style="red3")
+
         plot.refresh()
         self._phi += 0.1
 
