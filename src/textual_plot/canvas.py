cbed82e471588c8c4b799d4459bdf1ec207ffba5
David Fokkema <davidfokkema@icloud.com>
2025-01-25 00:12:56 +0100

    Parse markup when writing text

--- src/textual_plot/canvas.py
+++ src/textual_plot/canvas.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 
 from rich.segment import Segment
 from rich.style import Style
+from rich.text import Text
 from textual import on
 from textual._box_drawing import BOX_CHARACTERS
 from textual.app import App, ComposeResult
@@ -126,22 +127,34 @@ class Canvas(Widget):
         if y < 0 or y >= self._canvas_size.height:
             return
 
+        # parse markup
+        rich_text = Text.from_markup(text)
+        # store plain text
+        plain_text = rich_text.plain
+        # store styles for each individual character
+        rich_styles = []
+        for c in rich_text.divide(range(1, len(plain_text))):
+            style = Style()
+            for span in c._spans:
+                style += Style.parse(span.style)
+            rich_styles.append(style)
+
         if align == TextAlign.RIGHT:
-            x -= len(text) - 1
+            x -= len(plain_text) - 1
         elif align == TextAlign.CENTER:
-            div, mod = divmod(len(text), 2)
+            div, mod = divmod(len(plain_text), 2)
             x -= div
             if mod == 0:
                 # even number of characters, shift one to the right since I just
                 # like that better -- DF
                 x += 1
 
-        if x <= -len(text) or x >= self._canvas_size.width:
+        if x <= -len(plain_text) or x >= self._canvas_size.width:
             # no part of text falls inside the canvas
             return
 
         overflow_left = -x
-        overflow_right = x + len(text) - self._canvas_size.width
+        overflow_right = x + len(plain_text) - self._canvas_size.width
         if overflow_left > 0:
             buffer_left = 0
             text_left = overflow_left
@@ -152,10 +165,13 @@ class Canvas(Widget):
             buffer_right = None
             text_right = -overflow_right
         else:
-            buffer_right = x + len(text)
+            buffer_right = x + len(plain_text)
             text_right = None
 
-        self._buffer[y][buffer_left:buffer_right] = text[text_left:text_right]
+        self._buffer[y][buffer_left:buffer_right] = plain_text[text_left:text_right]
+        self._styles[y][buffer_left:buffer_right] = [
+            str(s) for s in rich_styles[text_left:text_right]
+        ]
         assert len(self._buffer[y]) == self._canvas_size.width
         assert len(self._styles[y]) == self._canvas_size.width
 
@@ -201,29 +217,29 @@ class Canvas(Widget):
 class DemoApp(App[None]):
     def compose(self) -> ComposeResult:
         yield (canvas := Canvas(60, 20))
-        canvas.draw_rectangle_box(2, 10, 10, 2, thickness=2)
         canvas.draw_line(0, 0, 8, 8)
         canvas.draw_line(0, 19, 39, 0, char="X", style="red")
+        canvas.draw_rectangle_box(2, 12, 40, 4, thickness=2)
         canvas.write_text(-2, 0, "".join([str(i % 10) for i in range(70)]))
         canvas.write_text(
             -2,
             1,
-            "Hi [bold]there[/bold], are you [green]ok[/green]?",
+            "[italic bold purple]Hi[/] [bold]there[/bold], are you [green]o[bold]k[/green]?[/]?",
         )
         canvas.write_text(
             1,
             2,
-            "Hi [bold]there[/bold], are you [green]ok[/green]?",
+            "[italic bold purple]Hi[/] [bold]there[/bold], are you [green]o[bold]k[/green]?[/]?",
         )
         canvas.write_text(
             30,
             1,
-            "Hi [bold]there[/bold], are you [green]ok[/green]?",
+            "[italic bold purple]Hi[/] [bold]there[/bold], are you [green]o[bold]k[/green]?[/]?",
         )
         canvas.write_text(
             38,
             1,
-            "Hi [bold]there[/bold], are you [green]ok[/green]?",
+            "[italic bold purple]Hi[/] [bold]there[/bold], are you [green]o[bold]k[/green]?[/]?",
         )
 
     # @on(Canvas.Resize)
