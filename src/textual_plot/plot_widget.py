a7130e400e55ed8fbfb38d5c54fb5ff1e2d36791
David Fokkema <davidfokkema@icloud.com>
2025-01-25 21:30:25 +0100

    Prune PlotWidget and use Canvas

--- src/textual_plot/plot_widget.py
+++ src/textual_plot/plot_widget.py
@@ -10,11 +10,14 @@ from textual import on
 from textual._box_drawing import BOX_CHARACTERS
 from textual.app import App, ComposeResult
 from textual.color import Color
+from textual.containers import Grid, Horizontal
 from textual.events import MouseScrollDown, MouseScrollUp
 from textual.geometry import Region, Size
 from textual.strip import Strip
 from textual.widget import Widget
 from textual.widgets import Placeholder
+
+from textual_plot.canvas import Canvas
 
 ZOOM_FACTOR = 0.01
 
@@ -43,20 +46,46 @@ class TextAlign(enum.Enum):
 
 
 class PlotWidget(Widget):
+    DEFAULT_CSS = """
+        PlotWidget {
+            Grid {
+                grid-size: 2 2;
+
+                #bottom-margin {
+                    column-span: 2;
+                }
+            }
+        }
+    """
+
     _datasets: list[DataSet]
 
     _x_min: float = 0.0
     _x_max: float = 10.0
     _y_min: float = 0.0
     _y_max: float = 30.0
-    _margin_top: int = 2
-    _margin_right: int = 5
+    _margin_top: int = 1
+    _margin_right: int = 1
     _margin_bottom: int = 5
     _margin_left: int = 10
 
     def __init__(self, id: str | None = None) -> None:
         super().__init__(id=id)
         self.clear()
+
+    def compose(self) -> ComposeResult:
+        with Grid():
+            yield Canvas(id="left-margin")
+            yield Canvas(id="plot")
+            yield Canvas(id="bottom-margin")
+
+    def on_mount(self) -> None:
+        self._update_margin_sizes()
+
+    def _update_margin_sizes(self) -> None:
+        grid = self.query_one(Grid)
+        grid.styles.grid_columns = f"{self._margin_left} 1fr"
+        grid.styles.grid_rows = f"1fr {self._margin_bottom}"
 
     def clear(self) -> None:
         self._datasets = []
@@ -95,41 +124,6 @@ class PlotWidget(Widget):
         self._render_plot()
         return super().refresh(
             *regions, repaint=repaint, layout=layout, recompose=recompose
-        )
-
-    def _linear_mapper(
-        self,
-        x: float | int,
-        a: float | int,
-        b: float | int,
-        a_prime: float | int,
-        b_prime: float | int,
-    ) -> float:
-        # FIXME: needs adjustment because I want 'outer' limits
-        return a_prime + (x - a) * (b_prime - a_prime) / (b - a)
-
-    def _map_coordinates_to_pixels(
-        self,
-        x: list[float] | NDArray[np.floating],
-        y: list[float] | NDArray[np.floating],
-    ) -> tuple[list[int], list[int]]:
-        return (
-            [
-                round(
-                    self._linear_mapper(
-                        px, self._x_min, self._x_max, 0, self._plot_size.width - 1
-                    )
-                )
-                for px in x
-            ],
-            [
-                round(
-                    self._linear_mapper(
-                        py, self._y_min, self._y_max, 0, self._plot_size.height - 1
-                    )
-                )
-                for py in y
-            ],
         )
 
     def _render_plot(self) -> None:
@@ -175,150 +169,6 @@ class PlotWidget(Widget):
                     # data point is outside plot area
                     pass
 
-    def bresenham_line(
-        self, x0: int, y0: int, x1: int, y1: int
-    ) -> Iterator[tuple[int, int]]:
-        """Get all pixel coordinates on the line between two points.
-
-        Algorithm was taken from
-        https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm and
-        translated to Python.
-
-        Args:
-            x0: starting point x coordinate
-            y0: starting point y coordinate
-            x1: end point x coordinate
-            y1: end point y coordinate
-
-        Yields:
-            Tuples of (x, y) coordinates that make up the line.
-        """
-        dx = abs(x1 - x0)
-        sx = 1 if x0 < x1 else -1
-        dy = -abs(y1 - y0)
-        sy = 1 if y0 < y1 else -1
-        error = dx + dy
-
-        while True:
-            yield x0, y0
-            e2 = 2 * error
-            if e2 >= dy:
-                if x0 == x1:
-                    break
-                error = error + dy
-                x0 = x0 + sx
-            if e2 <= dx:
-                if y0 == y1:
-                    break
-                error = error + dx
-                y0 = y0 + sy
-
-    def render_line(self, y: int) -> Strip:
-        """Render a line of the widget. y is relative to the top of the widget."""
-        # calculate rows and ranges of parts
-        rows_plot = (
-            self._margin_top,
-            self.size.height - self._margin_bottom - 1,
-        )
-        row_top_border = rows_plot[0] - 1
-        row_bottom_border = rows_plot[1] + 1
-
-        # render parts
-        if y < row_top_border:
-            return self._render_top_margin()
-        elif y == row_top_border:
-            return self._render_box_top()
-        elif rows_plot[0] <= y <= rows_plot[1]:
-            return self._render_plot_area_with_leftright_margins(idx=y - rows_plot[0])
-        elif y == row_bottom_border:
-            return self._render_box_bottom()
-        else:
-            return self._render_bottom_margin(idx=y - (row_bottom_border + 1))
-
-    def _render_top_margin(self) -> Strip:
-        return Strip(
-            [Segment(" " * self.size.width, Style(bgcolor="red"))],
-            cell_length=self.size.width,
-        )
-
-    def _render_box_top(self) -> Strip:
-        left_margin = insert_text_into(
-            " " * (self._margin_left - 1),
-            f"{self._y_max:.3f}",
-            index=self._margin_left - 3,
-            align=TextAlign.RIGHT,
-        )
-
-        get_box = BOX_CHARACTERS.__getitem__
-        return Strip(
-            [
-                Segment(left_margin, Style.parse("bold white on black")),
-                Segment(
-                    get_box((0, 2, 2, 2))
-                    + get_box((0, 2, 0, 2)) * self._plot_size.width
-                    + get_box((0, 0, 2, 2))
-                ),
-                Segment(" " * (self._margin_right - 1), Style(bgcolor="green")),
-            ]
-        )
-
-    def _render_plot_area_with_leftright_margins(self, idx: int) -> Strip:
-        get_box = BOX_CHARACTERS.__getitem__
-        left_margin = " " * (self._margin_left - 1)
-        return Strip(
-            [Segment(left_margin, Style(bgcolor="blue"))]
-            + ([Segment(get_box((2, 0, 2, 0)))] if self._margin_left else [])
-            + self._canvas[self._plot_size.height - 1 - idx]
-            + ([Segment(get_box((2, 0, 2, 0)))] if self._margin_right else [])
-            + [
-                Segment(" " * (self._margin_right - 1), Style(bgcolor="blue")),
-            ],
-            cell_length=self.size.width,
-        )
-
-    def _render_box_bottom(self) -> Strip:
-        left_margin = insert_text_into(
-            " " * (self._margin_left - 1),
-            f"{self._y_min:.3f}",
-            index=self._margin_left - 3,
-            align=TextAlign.RIGHT,
-        )
-
-        get_box = BOX_CHARACTERS.__getitem__
-        return Strip(
-            [
-                Segment(left_margin, Style.parse("bold white on black")),
-                Segment(
-                    get_box((2, 2, 2, 2))
-                    + get_box((0, 2, 0, 2)) * self._plot_size.width
-                    + get_box((2, 0, 2, 2))
-                ),
-                Segment(" " * (self._margin_right - 1), Style(bgcolor="green")),
-            ]
-        )
-
-    def _render_bottom_margin(self, idx: int) -> Strip:
-        if idx == 0:
-            tick_labels = " " * self.size.width
-            tick_labels = insert_text_into(
-                tick_labels,
-                str(self._x_min),
-                index=self._margin_left - 1,
-                align=TextAlign.CENTER,
-            )
-            tick_labels = insert_text_into(
-                tick_labels,
-                str(self._x_max),
-                index=self.size.width - self._margin_right,
-                align=TextAlign.RIGHT,
-            )
-            return Strip([Segment(tick_labels, Style.parse("bold white on black"))])
-        else:
-            return Strip(
-                [Segment(" " * self.size.width, Style(bgcolor="red"))],
-                cell_length=self.size.width,
-            )
-
     @on(MouseScrollDown)
     def zoom_in(self, event: MouseScrollDown) -> None:
         if (offset := event.get_content_offset(self)) is not None:
@@ -347,80 +197,11 @@ class PlotWidget(Widget):
         return xp, yp
 
 
-def insert_text_into(
-    string: str, text: str, index: int, align: TextAlign = TextAlign.LEFT
-) -> str:
-    """Insert text into a string, overwriting the original string.
-
-    This method will insert text into an existing string, overwriting the
-    original and keeping its length, clipping the text if necessary. You can
-    specify the text alignment (left, center or right).
-
-    For example:
-
-        >>> insert_text_into("0123456789", "---", 0)
-        '---3456789'
-        >>> insert_text_into("0123456789", "---", 4)
-        '0123---789'
-        >>> insert_text_into("0123456789", "---", 9)
-        '012345678-'
-        >>> insert_text_into("0123456789", "---", 20)
-        '0123456789'
-        >>> insert_text_into("0123456789", "---", -1)
-        '--23456789'
-        >>> insert_text_into("0123456789", "---", -2)
-        '-123456789'
-        >>> insert_text_into("0123456789", "---", -20)
-        '0123456789'
-        >>> insert_text_into("0123456789", "---", 9, align=TextAlign.RIGHT)
-        '0123456---'
-
-    Args:
-        string: The original string.
-        text: The text to insert.
-        index: The position at which to insert.
-        alignment: The text alignment (left, center or right).
-
-    Returns:
-        The new string.
-    """
-    if align == TextAlign.RIGHT:
-        index -= len(text) - 1
-    elif align == TextAlign.CENTER:
-        div, mod = divmod(len(text), 2)
-        index -= div
-        if mod == 0:
-            # even number of characters, shift one to the right since I just
-            # like that better -- DF
-            index += 1
-
-    if index >= len(string) or index <= -len(text):
-        return string
-    elif index < 0:
-        return text[-index:] + string[len(text) + index :]
-    else:
-        return (
-            string[:index] + text[: len(string) - index] + string[index + len(text) :]
-        )
-
-
 class DemoApp(App[None]):
-    CSS = """
-        PlotWidget {
-            height: 2fr;
-            border: solid $secondary;
-            padding: 2;
-        }
-        Placeholder {
-            height: 1fr;
-        }
-    """
     _phi: float = 0.0
 
     def compose(self) -> ComposeResult:
-        # yield Placeholder()
         yield PlotWidget()
-        # yield Placeholder()
 
     def on_mount(self) -> None:
         self.set_interval(1 / 24, self.plot_refresh)
@@ -428,22 +209,20 @@ class DemoApp(App[None]):
     def plot_refresh(self) -> None:
         plot = self.query_one(PlotWidget)
         plot.clear()
-        plot.scatter(
-            x=[0, 1, 2, 3, 4, 5, 9.99],
-            y=[0, 1, 4, 9, 16, 25, 29.99],
-            marker_style="blue",
-            marker="*",
-        )
-        x = np.linspace(0, 10, 17)
-        plot.plot(x=x, y=10 + 10 * np.sin(x + self._phi), line_style="red3")
-        plot.plot(x=x, y=10 + 10 * np.sin(x + self._phi + 1), line_style="green")
+        # plot.scatter(
+        #     x=[0, 1, 2, 3, 4, 5, 9.99],
+        #     y=[0, 1, 4, 9, 16, 25, 29.99],
+        #     marker_style="blue",
+        #     marker="*",
+        # )
+        # x = np.linspace(0, 10, 17)
+        # plot.plot(x=x, y=10 + 10 * np.sin(x + self._phi), line_style="red3")
+        # plot.plot(x=x, y=10 + 10 * np.sin(x + self._phi + 1), line_style="green")
 
-        plot.refresh()
-        self._phi += 0.1
+        # plot.refresh()
+        # self._phi += 0.1
 
 
 if __name__ == "__main__":
     app = DemoApp()
     app.run()
-    # p = PlotWidget()
-    # print(f"{p._linear_mapper(1.2, 0, 1.0, 0, 10)=}")
