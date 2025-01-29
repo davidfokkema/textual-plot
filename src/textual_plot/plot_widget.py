6f436be6b93d855698e19735236edc70c49215cb
David Fokkema <davidfokkema@icloud.com>
2025-01-29 16:57:26 +0100

    Fix drag jumping bug

--- src/textual_plot/plot_widget.py
+++ src/textual_plot/plot_widget.py
@@ -281,6 +281,15 @@ class PlotWidget(Widget):
     @on(MouseMove)
     def drag_plot(self, event: MouseMove) -> None:
         if event.button == 0:
+            # If no button is pressed, don't drag.
+            return
+        if event.screen_offset == (0, 0) or (
+            event.x == event.delta_x or event.y == event.delta_y
+        ):
+            # FIXME: BUG in Textual?
+            # If the mouse moves out of the window, the position jumps to (0, 0)
+            # briefly, creating large offsets equal to the current position in
+            # the next move event.
             return
         x1, y1 = self.get_coordinate_from_pixel(1, 1)
         x2, y2 = self.get_coordinate_from_pixel(2, 2)
